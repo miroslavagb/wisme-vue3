518e187de9f679b874554a138a5b4b3c8a896fa7
miroslavagb <miroslava.babacheva@gmail.com>
2025-02-06 14:19:07 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [
     vue(),
-    
   ],
   resolve: {
     alias: {
@@ -14,5 +13,16 @@ export default defineConfig({
       'https://json.schemastore.org/prettierrc': path.resolve(__dirname, 'schemas/prettierrc.json'),
       'https://json.schemastore.org/jsconfig': path.resolve(__dirname, 'schemas/jsconfig.json'),
     },
+  },
+  server: {
+    host: true, 
+    port: 5173, 
+  },
+  preview: {
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'lionfish-app-v3fsi.ondigitalocean.app', 
+    ],
   },
 });
